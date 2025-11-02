21f70f6d14c6a321466099e6392fabd85d130603
ataur65 <ataur.rahman6846@gmail.com>
2025-11-02 18:49:47 +0600

    fix: Correct authOptions import in stripe route

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import Order from '@/lib/models/Order';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
